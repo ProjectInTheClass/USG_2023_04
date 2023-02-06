39117c7541f2a2c519996b8bfcc357630df8e991
백대홍 <bdh3620@naver.com>
2023-02-06 21:43:42 +0900

    Netfulix file add
    새로운 파일로 생성

---
+++ Netfulix Clone/Netfulix Clone/Netfulix_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Netfulix_CloneApp.swift
+//  Netfulix Clone
+//
+//  Created by 백대홍 on 2023/02/06.
+//
+
+import SwiftUI
+
+@main
+struct Netfulix_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
